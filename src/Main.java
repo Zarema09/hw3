6f5cc41cb8390d269ff059cec998707d30bde604
Zarema09 <omoraalieva.09@icloud.com>
2024-09-10 18:58:05 +0600

    hw3

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        double[] numbers = {1.4, 5.3, -3.2, -7.6, 3.2, 1.3, 2.6, -8.9, 9.6, -5.8, 3.9, -7.1, 4.0, 8.1, 8.4, 8.6};
+        boolean negative = false;
+        double sum = 0;
+        int counter = 0;
+        for (double number : numbers) {
+            if (negative && number > 0) {
+                sum += number;
+                counter++;
+            }
+            if (number < 0 ){
+                negative = true;
+            }
         }
+        double average = sum / counter;
+        System.out.println(average);
     }
 }
